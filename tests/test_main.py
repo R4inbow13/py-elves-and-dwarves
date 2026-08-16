f0f56e4f065f20832ef212d141409620a2f243db
Stas Makarov <makarov.st@icloud.com>
2026-08-16 16:44:46 +0300

    correction2

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,10 +8,10 @@ from app import main
 from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
 from app.players.dwarves.dwarf import Dwarf
 from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
-from app.players import DwarfWarrior
-from app.players.elves import Druid
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.elves.druid import Druid
 from app.players.elves.elf import Elf
-from app.players.elves import ElfRanger
+from app.players.elves.elf_ranger import ElfRanger
 from app.players.player import Player
 
 
